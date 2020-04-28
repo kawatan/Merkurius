1e8009c1ca767fa646cdc3686ab2f504cf9df630
Milchchan <m1lchch4n@gmail.com>
2020-04-29 05:30:12 +0900

    Add Seq2seq Test

---
+++ Seq2seqTest/Decoder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Merkurius;
+using Merkurius.Layers;
+
+namespace Seq2seqTest
+{
+    [DataContract]
+    public class Decoder
+    {
+        private Embedding embedding = null;
+        private LSTM lstm = null;
+        private FullyConnected fullyConnected = null;
+        
+        public Decoder(int sequenceLength, int vocabularySize, int wordVectorSize, int hiddenSize = 256)
+        {
+            this.embedding = new Embedding(sequenceLength, vocabularySize, wordVectorSize, (fanIn, fanOut) => 0.01 * Initializers.LeCunNormal(fanIn));
+            this.lstm = new LSTM(wordVectorSize, hiddenSize, sequenceLength, true, false, (fanIn, fanOut) => Initializers.LeCunNormal(fanIn));
+            this.fullyConnected = new FullyConnected(hiddenSize, sequenceLength, sequenceLength * vocabularySize, (fanIn, fanOut) => Initializers.LeCunNormal(fanIn));
+        }
+    }
+}
---
+++ Seq2seqTest/Seq2seq.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using Merkurius;
+using Merkurius.ActivationFunctions;
+using Merkurius.Layers;
+
+namespace Seq2seqTest
+{
+    [DataContract]
+    public class Seq2seq : Layer, IUpdatable
+    {
+        public double[] Weights { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Seq2seq() : base(0, 0)
+        {
+
+        }
+
+        public override Batch<double[]> Forward(Batch<double[]> inputs, bool isTraining)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Batch<double[]> Backward(Batch<double[]> deltas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Batch<double[]> GetGradients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetGradients(Func<bool, double, int, double> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Batch<double[]> gradients, Func<double, double, double> func)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
